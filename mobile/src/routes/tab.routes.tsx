081983bcba63cad172cb74af36cd19867a93b3b1
Caio Moraes V Santos <caio.moraes@radixeng.com.br>
2023-03-31 17:56:59 -0300

    :paint: [Frontend] - React navigate adjust style

--- mobile/src/routes/tab.routes.tsx
+++ mobile/src/routes/tab.routes.tsx
@@ -7,7 +7,7 @@ import { Config } from '../components/Config';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 const navTheme = DefaultTheme;
-navTheme.colors.background = colors.zinc[800];
+navTheme.colors.background = colors.zinc[900];
 
 const { Navigator, Screen } = createMaterialTopTabNavigator();
 
@@ -23,15 +23,17 @@ export function TopTabs() {
                 tabBarBounces: true,
 
                 tabBarIndicatorStyle: {
-                    // backgroundColor: colors.indigo[400],
-                    backgroundColor: "transparent",
+                    backgroundColor: colors.indigo[400],
+                    // backgroundColor: "transparent",
                 },
 
                 tabBarPressColor: "transparent",
 
                 tabBarStyle: {
                     backgroundColor: colors.zinc[800],
+                    
                     paddingTop: 50,
+
                     borderWidth: 0,
                     elevation: 0,
                 },
